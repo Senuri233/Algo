600dac1b5da3241807cf1e31dad0702d371524ca
Senuri233 <senuri676@gmail.com>
2026-07-09 12:25:43 +0530

    Add files via upload

---
+++ algoq4.py
@@ -0,0 +1,8 @@
+array = [10,20,30,40,50]
+
+element = int(input("enter the element that you need to update: "))
+index = int(input("enter the index at which you need to update the element: "))
+
+array[index] = element
+print(array)
+
