1d32f651ce4d06be830cfccc76bd035bbf086ad9
Senuri233 <senuri676@gmail.com>
2026-07-09 12:17:21 +0530

    Add files via upload

---
+++ algoq1.py
@@ -0,0 +1,5 @@
+array = []
+array = [10, 20, 30, 40, 50, 60, 70, 80, 80, 100]
+
+for i in range(len(array)):
+    print(array[i])
---
+++ algoq2.py
@@ -0,0 +1,9 @@
+array = []
+array = [10,20,30,40,50]
+
+element = int(input("enter the element that you need to insert: "))
+index = int(input("enter the index at which you need to insert the element: "))
+
+array.insert(index, element)
+print(array)
+
